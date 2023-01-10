432f0624ecf0e83b633110c7a632dababc1970ca
owl <arifikhwanhendra2@gmail.com>
2023-01-10 23:01:53 +0700

    more on server from scratch

--- server/intro_2/nodejs_server.js
+++ server/intro_2/nodejs_server.js
@@ -1,4 +1,40 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 exports.__esModule = true;
 // https://medium.com/swlh/how-to-build-a-javascript-web-server-from-scratch-using-only-node-js-a4e21a9abc88
 // An HTTP header is essentially a set of key/value
@@ -7,33 +43,49 @@ exports.__esModule = true;
 // side or the “server” side.
 var http = require("http");
 var url_module = require("url");
+var fs = require("fs");
 // SERVER VARS
 var PORT = 3000;
 http
-    .createServer(function (request, response) {
-    // REQUEST
-    var url = request.url, method = request.method, headers = request.headers;
-    // headers
-    var host = headers.host;
-    //
-    var url_parsed;
-    if (url !== undefined) {
-        url_parsed = url_module.parse(url, true);
-    }
-    // let query;
-    // if (url_parsed) {
-    //   query = url_parsed.query;
-    // }
-    // const { name } = url_parsed?.query!;
-    // console.log(url_parsed?.query.name);
-    // console.log(url_parsed);
-    // console.log(query);
-    // console.log(url);
-    // console.log(request);
-    // response.setHeader("Content-Type", "text/plain");
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    response.end("\nHELLO ...\nMethod: ".concat(method, "\nurl ").concat(url, "\nhost: ").concat(host, "\nname query: ").concat((url_parsed === null || url_parsed === void 0 ? void 0 : url_parsed.query.name) ? url_parsed === null || url_parsed === void 0 ? void 0 : url_parsed.query.name : "no query"));
-})
+    .createServer(function (request, response) { return __awaiter(void 0, void 0, void 0, function () {
+    var url, method, headers, host, url_parsed, html;
+    return __generator(this, function (_a) {
+        url = request.url, method = request.method, headers = request.headers;
+        host = headers.host;
+        if (url !== undefined) {
+            url_parsed = url_module.parse(url, true);
+        }
+        // let query;
+        // if (url_parsed) {
+        //   query = url_parsed.query;
+        // }
+        // const { name } = url_parsed?.query!;
+        // console.log(url_parsed?.query.name);
+        // console.log(url_parsed);
+        // console.log(query);
+        // console.log(url);
+        // console.log(request);
+        // response.setHeader("Content-Type", "text/plain");
+        if (url === "/") {
+            response.writeHead(200, { "Content-Type": "text/plain" });
+            return [2 /*return*/, response.end("ROOT /")];
+        }
+        if (url === "/home") {
+            response.writeHead(200, { "Content-Type": "text/html" });
+            html = fs.readFileSync("intro.html", "utf8");
+            return [2 /*return*/, response.end(html)];
+            //
+            // fs.readFile("intro.html", (error, data) => {
+            //   // response.writeHead(200, { "Content-Type": "text/html" });
+            //   // response.write(data);
+            //   // return response.end();
+            //   console.log(data);
+            // });
+        }
+        response.writeHead(200, { "Content-Type": "text/plain" });
+        return [2 /*return*/, response.end("DEFAULT URL\nHELLO ...\nMethod: ".concat(method, "\nurl ").concat(url, "\nhost: ").concat(host, "\nname query: ").concat((url_parsed === null || url_parsed === void 0 ? void 0 : url_parsed.query.name) ? url_parsed === null || url_parsed === void 0 ? void 0 : url_parsed.query.name : "no query"))];
+    });
+}); })
     .listen(PORT, function () {
     console.log("SERVER IS RUNNING ON PORT", PORT);
 });
--- server/intro_2/nodejs_server.ts
+++ server/intro_2/nodejs_server.ts
@@ -5,12 +5,13 @@
 // side or the “server” side.
 import * as http from "http";
 import * as url_module from "url";
+import * as fs from "fs";
 
 // SERVER VARS
 const PORT: number = 3000;
 
 http
-  .createServer((request, response) => {
+  .createServer(async (request, response) => {
     // REQUEST
     const { url, method, headers } = request;
     // headers
@@ -31,9 +32,25 @@ http
     // console.log(url);
     // console.log(request);
     // response.setHeader("Content-Type", "text/plain");
+    if (url === "/") {
+      response.writeHead(200, { "Content-Type": "text/plain" });
+      return response.end("ROOT /");
+    }
+    if (url === "/home") {
+      response.writeHead(200, { "Content-Type": "text/html" });
+      const html = fs.readFileSync("intro.html", "utf8");
+      return response.end(html);
+      //
+      // fs.readFile("intro.html", (error, data) => {
+      //   // response.writeHead(200, { "Content-Type": "text/html" });
+      //   // response.write(data);
+      //   // return response.end();
+      //   console.log(data);
+      // });
+    }
     response.writeHead(200, { "Content-Type": "text/plain" });
-    response.end(
-      `\nHELLO ...\nMethod: ${method}\nurl ${url}\nhost: ${host}\nname query: ${
+    return response.end(
+      `DEFAULT URL\nHELLO ...\nMethod: ${method}\nurl ${url}\nhost: ${host}\nname query: ${
         url_parsed?.query.name ? url_parsed?.query.name : "no query"
       }`
     );
